02bf30a75e8ae00205a5aff1c9bef00914971296
lakota <lakotajcamp@gmail.com>
2024-01-10 04:36:04 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+# - GET /messages: returns an array of all messages as
+#   JSON, ordered by created_at in ascending order.
+# - POST /messages: creates a new message with a body 
+#   and username from params, and returns the newly 
+#   created post as JSON.
+# - PATCH /messages/<int:id>: updates the body of the 
+#   message using params, and returns the updated 
+#   message as JSON.
+# - DELETE /messages/<int:id>: deletes the message from
+#   the database.
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        message_get = Message.query.order_by(Message.created_at).all()
+        message_get_list = [msg.to_dict() for msg in message_get]
+        return make_response(message_get_list,200)
+    elif request.method == 'POST':
+        data = request.get_json()
+        message_post = Message(
+            username=data["username"],
+            body=data["body"]
+        )
+        db.session.add(message_post)
+        db.session.commit()
+        return make_response(message_post.to_dict(),201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    if request.method == 'PATCH':
+        message_patch = Message.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr in data:
+            setattr(message_patch, attr, data[attr])
+        db.session.add(message_patch)
+        db.session.commit()
+        message_patch_dict = message_patch.to_dict()
+        return make_response(message_patch_dict, 200)
+    elif request.method == 'DELETE':
+        message_delete = Message.query.filter_by(id=id).first()
+        db.session.delete(message_delete)
+        db.session.commit()
+        return make_response({'message':'post deleted'}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -8,7 +8,20 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+# - Start by generating the Message model and the
+#   necessary migration code to create messages with
+#   the following attributes:
+# - "body": String.
+# - "username": String.
+# - "created_at": DateTime.
+# - "updated_at": DateTime.
+# - Don't forget to add default values for "created_at" and "updated_at"!
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
